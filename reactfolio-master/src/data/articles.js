18e052dbb7770cdabc6a07713decb5762ef81ca0
Varun1802 <60618498+Varun1802@users.noreply.github.com>
2024-01-30 05:16:30 -0800

    Final

---
+++ reactfolio-master/src/data/articles.js
@@ -0,0 +1,100 @@
+
+const  articles= [{
+		date: "07 Dec, 2021",
+		title: "Feel of a thriller, sweep of a family saga",
+		description:
+			"There have been very few debuts that have been endorsed unanimously by all three heavyweights writers in one go—Shobha De, Shashi Tharoor and Chita Divakaruni. Even fewer live up to the high praise, but Shivani Sibal does—and—more so admirably.",
+		links:"https://www.theweek.in/review/books/2021/12/07/shivani-sibals-equations-has-feel-of-a-thriller-sweep-of-a-family-saga.html"
+	},
+	{
+		date:"17 Nov, 2021",
+		title:"A compelling gaze",
+		description:"In conversation with Medha Dutta Yadav, Shivani Sibal talks about the layers that form the theme in her debut novel, and how it is a simple story of two friends, and their families.",
+		links:"https://www.newindianexpress.com/magazine/2021/Jul/18/equations-book-review-a-compelling-gaze-2330735.html"
+	  },
+	{
+		date:"24 Sep, 2021",
+		title:"Capital Cabal",
+		description:"SHIVANI SIBAL’S DEBUT novel Equations is a firecracker. It is the kind of book that makes you want to read without pause.",
+		links:"https://openthemagazine.com/lounge/books/capital-cabal/",
+	  },
+	  {
+		date:"29 Aug, 2021",
+		title:"Blurs lines of power",
+		description:"A tale of sons of master and servant, Shivani Sibal’s debut novel ‘Equations’ revolves around the lives of Aahan Sikand, scion of the wealthy Sikand family, and Rajesh Kumar, son of Sikand’s driver.",
+		links:"https://www.tribuneindia.com/news/book-reviews/shivani-sibals-debut-novel-equations-blurs-lines-of-power-303474",
+	  },
+	  {
+		date:"25 Aug, 2021",
+		title:"About the conflict between Delhi's elite and the oppressed",
+		description:"In 2008, the Booker Prize catapulted Aravind Adiga’s novel The White Tiger to international attention and spawned a whole new sub-genre of cringe fiction.",
+		links:"https://lifestyle.livemint.com/how-to-lounge/books/a-driver-s-son-takes-on-delhi-s-rich-in-this-blah-first-novel-111629868715126.html",
+	  },
+	{
+		date: "14 Aug, 2021",
+		title: "Shivani Sibal’s novel is an engaging romp through Delhi’s streets of power",
+		description:
+			"Shivani Sibal’s Equations is a chronicle of power. It is a nuanced depiction of those born into it, those who acquire it, and those who have it taken away.",
+		links:"https://www.thehindu.com/books/ruthless-roads-percy-bharucha-reviews-equations-by-shivani-sibal/article35873980.ece"
+	},
+	{
+		date:"14 Aug, 2021",
+		title:"Shivani Sibal’s novel is an engaging romp through Delhi’s streets of power",
+		description:"Shivani Sibal’s Equations is a chronicle of power. It is a nuanced depiction of those born into it, those who acquire it, and those who have it taken away.",
+		links:"https://www.thehindu.com/books/ruthless-roads-percy-bharucha-reviews-equations-by-shivani-sibal/article35873980.ece",
+	  },
+	  {
+		date:"14 Aug, 2021",
+		title:"The telling of this story is as effective as the story itself.",
+		description:"A member of the domestic staff in my house muttered a thickly accented “sorry”, a word she had picked up from me, when I saw her sit on one of the dining chairs.",
+		links:"https://scroll.in/article/1002721/shivani-sibals-debut-novel-neatly-slices-open-class-equations-within-delhis-power-elite",
+	  },
+	  {
+		date:"06 Aug, 2021",
+		title:"Equations is an amazing journey of families",
+		description:"Equations is an amazing journey of families who grew with time in different situations. With a glimpse of situations in the 90s and the present, it’s a complete Bollywood story with layers of power, politics, and of course love.",
+		links:"https://vidhyathakkar.com/equations-by-shivani-sibal-book-review/",
+	  },
+	  {
+		date:"27 July, 2021",
+		title:"A portrait of 1980s Delhi and its aspirational inhabitants",
+		description:"Equations is a story of aspiration and social change, of individual ambition and family bonds, told through the lives of two families, the Sikands, a wealthy business clan, and the Kumars, who are the family of their domestic help.",
+		links:"https://www.firstpost.com/art-and-culture/page-turner-in-shivani-sibals-novel-equations-a-portrait-of-1980s-delhi-and-its-aspirational-inhabitants-9821051.html",
+	  },
+	{
+		date:"16 July, 2021",
+		title:"Review: Equations by Shivani Sibal",
+		description:"A memorable portrayal of social dynamics, Shivani Sibal’s novel is unsentimental, thoughtful and compassionate",
+		links:"https://www.hindustantimes.com/books/reviewequations-by-shivani-sibal-101626436818558.html"
+	  },
+
+	  {
+		date:"03 Feb, 2021",
+		title:"A story of individual ambition and fragile family bonds, of aspiration and inevitable social change",
+		description:"t is New Delhi in the 1980s, and Aahan Sikand and Rajesh Kumar are childhood playmates.",
+		links:"https://harpercollins.co.in/blog/announcements/press-release/equations-a-novel/",
+	  },
+	  {
+		date:"03 Feb, 2021",
+		title:"The act of writing makes you confront yourself",
+		description:"It is New Delhi in the 1980s, and Aahan Sikand and Rajesh Kumar are childhood playmates. They have been brought up under the care of the same woman, living in the same house, but with starkly different realities",
+		links:"https://thepunchmagazine.com/the-byword/interviews/shivani-sibal-amp-lsquo-the-act-of-writing-makes-you-confront-yourself-amp-rsquo",
+	  },
+	  {
+		date:"03 Feb, 2021",
+		title:"The drama of a now lost time unfold before our eyes frame by frame",
+		description:"A debut novel by Shivani Sibal takes readers back to the 1980s – to the politics of Lutyens’ Delhi, to the story of a traditional family caught in the throes of change.",
+		links:"https://indianexpress.com/article/books-and-literature/namita-gokhales-daughter-shivani-pens-debut-novel-7173011/",
+	  },
+  
+
+	  {
+		date:"04 Feb, 2021",
+		title:"To the politics of Lutyens' Delhi",
+		description:"Adebut novel by Shivani Sibal takes readers back to the 1980s - to the politics of Lutyens' Delhi, to the story of a traditional family caught in the throes of change.",
+		links:"https://www.frontlist.in/namita-gokhales-daughter-shivani-pens-debut-novel",
+	  }
+]
+
+
+export default articles;
---
+++ reactfolio-master/src/pages/articles.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect } from "react";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import Article from "../components/articles/article";
+
+import INFO from "../data/user";
+import myArticles from "../data/articles";
+
+import "./styles/articles.css";
+
+const Articles = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+
+	return (
+		<React.Fragment>
+	
+			<div className="page-content">
+				<NavBar active="articles" />
+				<div className="content-wrapper">
+					<div className="articles-logo-container">
+						<div className="articles-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+
+					<div className="articles-main-container">
+						<div className="flex flex-col md:flex-row">
+							<div>
+						<div className="title articles-title">
+							{INFO.articles.title}
+						</div>
+
+						<div className="subtitle articles-subtitle">
+							{INFO.articles.description}
+						</div>
+						</div>
+						{/*BOOK ANIMATION DIV*/}
+						<div className="book max-w-[600px] md:max-h-[100px]">
+						<div class="scene max-h-[470px] md:ml-20">
+  							  <div class="book-wrap">
+  							    <div class="left-side">
+  							      <div class="book-cover-left"></div>
+  							      <div class="layer1">
+  							        <div class="page-left"></div>
+  							      </div>
+  							      <div class="layer2">
+  							        <div class="page-left"></div>
+  							      </div>
+  							      <div class="layer3">
+  							        <div class="page-left"></div>
+  							      </div>
+  							      <div class="layer4">
+  							        <div class="page-left"></div>
+  							      </div>
+  							      <div class="layer-text">
+  							        <div class="page-left-2">
+  							          <div class="corner"></div>
+  							          <div class="corner2"></div>
+  							          <div class="corner-fold"></div>
+  							          <div class="page-text w-richtext text-sm ">
+  							            <h3><strong>June 2016</strong></h3>
+  							            <h6>BY <a href="" target="_blank">Shivani Sibal</a></h6>
+
+  							            <p className="text-sm">THE LAST OF the bags packed, Parul and Aahan Sikand were ready to leave 							their family home for the final time.Acquired in 1946, Sikand House was intended to be the 							cradle for many grandsons and great-grandsons in the coming years. Displaced as he was at 							Partition, Rai Bahadur Manohar Krishna Sikand, Aahan's grandfather, a largely self-made man</p>
+  							          </div>
+  							        </div>
+  							      </div>
+  							    </div>
+  							    <div class="center"></div>
+  							    <div class="right-side">
+  							      <div class="book-cover-right"></div>
+  							      <div class="layer1">
+  							        <div class="page-right"></div>
+  							      </div>
+  							      <div class="layer2 right">
+  							        <div class="page-right"></div>
+  							      </div>
+  							      <div class="layer3 right">
+  							        <div class="page-right"></div>
+  							      </div>
+  							      <div class="layer4 right">
+  							        <div class="page-right"></div>
+  							      </div>
+  							      <div class="layer-text right">
+  							        <div class="page-right-2">
+  							          <div class="page-text w-richtext">
+  							            <p>  with a premonition about difficult times ahead, didn't suffer the life of a refugee, 							having swapped his house with that of an equally astute Muslim acquaintance before all the 							madness began. It was a gentleman's agreement.</p>
+										  <p>The Rai Bahadur arrived in Delhi by air, with gold coins sewn into the lining of his and 							his wife's coats. Their worthy possessions had followed by road, including the heavy teak 							furniture and cast- iron cooking ...</p>
+  							          </div>
+  							        </div>
+  							      </div>
+  							    </div>
+  							  </div>
+  							</div>
+
+						</div>
+						</div>
+
+						<div className="articles-container">
+							<div className="articles-wrapper">
+								{myArticles.map((article, index) => (
+									<div
+										className="articles-article"
+										key={(index + 1).toString()}
+									>
+										<Article
+											key={(index + 1).toString()}
+											date={article.date}
+											title={article.title}
+											description={article.description}
+											links={article.links}
+										/>
+									
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Articles;
