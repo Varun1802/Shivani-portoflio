e26e92b7dae49fe956bfcbadc049add6f46fb861
Varun1802 <60618498+Varun1802@users.noreply.github.com>
2024-01-30 05:18:52 -0800

    Final

---
+++ reactfolio-master/src/data/user.js
@@ -0,0 +1,37 @@
+const INFO = {
+	main: {
+		title: "Shivani Sibal",
+		name: "Shivani Sibal Portfolio",
+		email: "shivanisibal@gmail.com",
+		logo: "../logo.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://www.instagram.com/shivani_sibal/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Shivani Sibal",
+		description:
+			"Shivani Sibal is a Delhi based author whose debut novel, Equations, was launched in July 2021. Set in New Delhi from the 1980s to modern times, the novel traces the story of social change, ambition and familial bonds. Equations has received rave reviews in several reputed publications, including The Hindustan Times, The Times of India, The Hindu, The New Indian Express, The Asian Age, Scroll, and Open Magazine.",
+	},
+
+	about: {
+		title: "I'm Shivani Sibal",
+		description:
+			"Shivani Sibal's acclaimed debut novel Equations heralds a fresh new voice in the world of contemporary Indian fiction. Set against the backdrop of 1980s Delhi, her narrative traverses the city’s transformation into the complexities of the modern era. Celebrated for its nuanced exploration of social dynamics, Equations has earned rave reviews from The Times of India, The Hindustan Times, The Hindu, The New Indian Express, The Asian Age, Scroll, The Week, and Open Magazine. She is working on her second novel.",
+	},
+
+	articles: {
+		title: "I'm passionate about writing and literature",
+		description:
+			"Chronological collection of the reviews on my Novel Equations by various Publishing houses and News Channel.",
+	},
+
+	
+};
+
+export default INFO;
