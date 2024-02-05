0a5cbaf7fb5d0006b601429a0c6a5b8f24dc1428
Varun <vc180201@gmail.com>
2024-02-05 23:10:17 +0530

    1440 x 1080 Responsive

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -49,12 +49,12 @@ const sendMail=( async function POST() {
 						</div>
 					</div>
 
-					<div className="contact-container flex flex-col justify-between items-center h-full">
-						<div className="text-4xl md:text-6xl ">
+					<div className="contact-container flex flex-col items-center h-screen">
+						<div className="text-4xl md:text-7xl ">
 							Let's Get in Touch:👋
 							</div>
-				<div className="md:w-1/2 self-center pt-8 mx-4">
-				<form action="" id="contact-form" className="flex flex-col gap-12 justify-center items-center ">
+				<div className="md:w-1/2 self-center mx-4">
+				<form action="" id="contact-form" className="flex flex-col gap-16 justify-between items-center ">
 					  <div class="input flex gap-2 justify-between items-center w-full">
 					    <User color="#14B8A6"/>
 							<input type="text" name="name" id="name" placeholder="Name" class="h-10 shadow-md drop-shadow-sm indent-4 rounded-lg w-full"  required />
